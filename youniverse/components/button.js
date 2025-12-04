96eda8cb9513fdbde38961d01ea785af625b411b
kyletria1 <kyleadriantria01@gmail.com>
2025-12-04 12:30:23 -0800

    add variant code for button.js

--- youniverse/components/button.js
+++ youniverse/components/button.js
@@ -1,9 +1,25 @@
 import styles from "./button.module.css"
 
-export default function Button() {
+export default function Button({
+    variant = "value",
+    valueText = "Click Me"
+}) { 
+    const labelMap = {
+    signUp: "Sign Up",
+    logIn: "Log In",
+    submit: "Submit",
+    logOut: "Log Out",
+    done: "Done",
+    continue: "Continue",
+    forgotPassword: "Forgot Password",
+    cancel: "Cancel",
+    value: valueText,
+    };
 
     return (
-        <button></button>
+        <button className={`${styles.button} ${styles["button--welcome"]} ${styles["button--variant"]}`}>
+            {labelMap[variant]}
+        </button>
     );
 
 }
