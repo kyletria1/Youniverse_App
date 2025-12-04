575015fb9c1a607f06c9cb11c9d5f500d49bd6b8
kyletria1 <kyleadriantria01@gmail.com>
2025-12-03 22:45:29 -0800

    change IntroPage to Intro and fix image path

--- youniverse/src/app/onboarding/intro/page.js
+++ youniverse/src/app/onboarding/intro/page.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
 
-export default function IntroPage() {
+export default function Intro() {
   const router = useRouter();
 
   useEffect(() => {
@@ -16,7 +16,7 @@ export default function IntroPage() {
 
   return (
     <div className={styles.container}>
-      <image src="./public/background_images/StarUpSpace Intro Background.svg"></image>
+      <image src="youniverse\public\background_images\StarUpSpace Intro Background.svg"></image>
     </div>
   );
 }
