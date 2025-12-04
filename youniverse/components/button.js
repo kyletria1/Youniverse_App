b1402b8c783554d23a03091625666022865981dd
kyletria1 <kyleadriantria01@gmail.com>
2025-12-04 13:35:43 -0800

    redo component so code is better designed

--- youniverse/components/button.js
+++ youniverse/components/button.js
@@ -1,25 +1,19 @@
-import styles from "./button.module.css"
+import styles from "./button.module.css";
 
 export default function Button({
-    variant = "value",
-    valueText = "Click Me"
-}) { 
-    const labelMap = {
-    signUp: "Sign Up",
-    logIn: "Log In",
-    submit: "Submit",
-    logOut: "Log Out",
-    done: "Done",
-    continue: "Continue",
-    forgotPassword: "Forgot Password",
-    cancel: "Cancel",
-    value: valueText,
-    };
+  variant = "interface", 
+  children,              
+  ...props
 
-    return (
-        <button className={`${styles.button} ${styles["button--welcome"]} ${styles["button--variant"]}`}>
-            {labelMap[variant]}
-        </button>
-    );
-
+}) {
+  return (
+    <button
+      className={`${styles.button} ${styles[`button--${variant}`]}`}
+      {...props}
+    >
+      <span className={styles.text}>
+        {children}
+      </span>
+    </button>
+  );
 }
