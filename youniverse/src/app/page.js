3c84dc8ec7acac8062524399f0af2f7458e0cc06
kyletria1 <kyleadriantria01@gmail.com>
2025-12-04 11:41:40 -0800

    add code explaination

--- youniverse/src/app/page.js
+++ youniverse/src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import {redirect} from "next/navigation";
+//tells next.js to not render this page but send user to another route
 
 export default function Home() {
   redirect("/onboarding/intro");
+  //instead of rendering this page the function send the user to the route of the intro page
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
