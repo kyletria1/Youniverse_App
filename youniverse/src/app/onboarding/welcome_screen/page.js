3ecc3482ca54b43e6748166de6800693019edc11
kyletria1 <kyleadriantria01@gmail.com>
2025-12-04 16:52:56 -0800

    fix issue with button component not loading

--- youniverse/src/app/onboarding/welcome_screen/page.js
+++ youniverse/src/app/onboarding/welcome_screen/page.js
@@ -2,7 +2,7 @@
 import {useRouter} from "next/navigation";
 import Image from "next/image";
 import styles from "./page.module.css"
-import Button from "button.js";
+import Button from "../../../../components/button";
 
 export default function Welcome() {
   const router = useRouter();
@@ -11,15 +11,17 @@ export default function Welcome() {
   return (
     <main className={`${styles.welcome} ${styles['welcome--fullscreen']}`}>
         <Image 
-        src="youniverse\public\background_images\WelcomeScreenBackground.svg" 
+        src="/background_images/WelcomeScreenBackground.svg" 
         alt="Youniverse Welcome Screen Background Image" 
         width={393}
         height={852}
         priority
         className={`${styles.welcome__image} ${styles['welcome__image--cover']}`}>
         </Image>
-        <Button variant="welcome">Sign Up</Button>
-        <Button variant="welcome">Log In</Button>
+        <div className={`${styles['authentication__buttons']} ${styles['authentication__buttons--stacked']}`}>
+          <Button variant="welcome">Sign Up</Button>
+          <Button variant="welcome">Log In</Button>
+        </div>
     </main>
   );
 }
