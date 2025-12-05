b3220649a73123ed858799b1a990112ba8914a4f
kyletria1 <kyleadriantria01@gmail.com>
2025-12-04 17:45:04 -0800

    complete button positioning (intro/welcome pages complete)

--- youniverse/src/app/onboarding/welcome_screen/page.js
+++ youniverse/src/app/onboarding/welcome_screen/page.js
@@ -9,19 +9,21 @@ export default function Welcome() {
   //[from code tutor] welcome page is mounted and router is grabbed again and now points to /onboarding/welcome_screen
 
   return (
-    <main className={`${styles.welcome} ${styles['welcome--fullscreen']}`}>
-        <Image 
-        src="/background_images/WelcomeScreenBackground.svg" 
-        alt="Youniverse Welcome Screen Background Image" 
-        width={393}
-        height={852}
-        priority
-        className={`${styles.welcome__image} ${styles['welcome__image--cover']}`}>
-        </Image>
-        <div className={`${styles['authentication__buttons']} ${styles['authentication__buttons--stacked']}`}>
+    <main className={styles.welcome}>
+      <div className={styles['welcome__frame']}>
+        <Image
+          src="/background_images/WelcomeScreenBackground.svg"
+          alt="Welcome Background"
+          width={393}
+          height={852}
+          priority
+          className={styles.welcome__image}
+        />
+        <div className={`${styles.authentication__buttons} ${styles['authentication__buttons--stacked']}`}>
           <Button variant="welcome">Sign Up</Button>
           <Button variant="welcome">Log In</Button>
         </div>
+      </div>
     </main>
   );
 }
