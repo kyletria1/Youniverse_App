3baf6e8b69a36d673712aa6a6209509a4d97cc2e
kyletria1 <kyleadriantria01@gmail.com>
2025-12-09 11:34:02 -0800

    position elements on planet interface screen

--- youniverse/src/app/planet/page.js
+++ youniverse/src/app/planet/page.js
@@ -2,21 +2,33 @@
 import styles from "./page.module.css"
 import Image from "next/image"
 import Lumen from "../../../components/Lumen";
+import SquareButton from "../../../components/SquareButton";
 import NavigationBar from "../../../components/NavigationBar";
 
 export default function Planet() {
     return(
-        <section className="screen">
-            <Image 
-                src="/background_images/PlanetInterfaceBackground.svg" 
-                alt="Youniverse Planet Interface Background" 
-                width={393}
-                height={852}
-                priority
-                className={`${styles.intro__image}`}>
-            </Image>
-            <Lumen/>
-            <NavigationBar variant="PlanetSelected"/>
-        </section>
+    <section className="screen">
+        <Image
+            src="/background_images/PlanetInterfaceBackground.svg"
+            alt="Youniverse Planet Interface Background"
+            width={393}
+            height={852}
+            priority
+            className={styles.intro__image}
+        />
+        <div className={styles.lumenWrapper}>
+            <Lumen />
+        </div>
+        <div className={styles.decorateButton}>
+            <SquareButton
+            variant="active"
+            iconSrc="/icons/button_icons/DecorateIcon.svg"
+            alt="Decorate Button"
+            />
+        </div>
+        <div className={styles.navigationBar}>
+            <NavigationBar variant="PlanetSelected" />
+        </div>
+    </section>
     );
 }
